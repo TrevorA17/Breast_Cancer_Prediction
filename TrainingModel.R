5baac701900f7b30d05543053d832c4a853bab95
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-05-29 19:04:15 +0300

    load dataset for training

--- TrainingModel.R
+++ TrainingModel.R
@@ -1,3 +1,22 @@
+# Load dataset
+breast_cancer_data <- read.csv("data/Breast_cancer_data.csv", colClasses = c(
+  mean_radius = "numeric",
+  mean_texture = "numeric",
+  mean_perimeter = "numeric",
+  mean_area = "numeric",
+  mean_smoothness = "numeric",
+  diagnosis = "factor"
+))
+
+# Display the structure of the dataset
+str(breast_cancer_data)
+
+# View the first few rows of the dataset
+head(breast_cancer_data)
+
+# View the dataset in a separate viewer window
+View(breast_cancer_data)
+
 # Install and load the caTools package
 install.packages("caTools")
 library(caTools)
